f56697c17093b4d954aa97564e0ad9445fde6f9c
Dikendev <diken.dev@gmail.com>
2024-07-11 20:58:53 -0300

    add schedule module / service / controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,20 @@ import { HouseModule } from './main/house/house.module';
 import { UserController } from './main/user/user.controller';
 import { UserModule } from './main/user/user.module';
 import { AppointmentModule } from './main/appointment/appointment.module';
+import { ScheduleModule } from './main/schedule/schedule.module';
+import { AppointmentController } from './main/appointment/appointment.controller';
+import { HouseController } from './main/house/house.controller';
+import { ScheduleController } from './main/schedule/schedule.controller';
 
 @Module({
-  imports: [HouseModule, UserModule, AppointmentModule],
-  controllers: [AppController, UserController],
+  imports: [AppointmentModule, HouseModule, ScheduleModule, UserModule],
+  controllers: [
+    AppController,
+    AppointmentController,
+    HouseController,
+    ScheduleController,
+    UserController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
