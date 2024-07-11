0e7ea075b2614d6cca9d9ec4278498fcefb4306a
Dikendev <diken.dev@gmail.com>
2024-07-11 20:58:59 -0300

    add tests

--- src/main/user/tests/user.service.spec.ts
+++ src/main/user/tests/user.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from './user.service';
-
+import { UserService } from '../user.service';
+import { UserDto } from '../dto/user-dto';
 describe('UserService', () => {
   let service: UserService;
 
@@ -14,5 +14,21 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a user', async () => {
+    const user: UserDto = {
+      name: 'John Doe',
+      email: 'john@doe',
+      password: 'password',
+      phone: '1234567890',
+      photo: 'http://photo',
+      role: 'admin',
+    };
+
+    const userResponse = await service.create(user);
+    expect(userResponse).toBeDefined();
+    expect(userResponse.id).toBe('1');
+    expect(service.inMemoryUsers.length).toBe(1);
   });
 });
